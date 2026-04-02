b185a4b91c1e0d31d3225030f1d6f3479b3ab2fc
samarbaike <samarb16k@gmail.com>
2026-04-03 00:00:29 +0600

    feat: done with invite code gen

--- app/routes/matches.py
+++ app/routes/matches.py
@@ -2,12 +2,14 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.database import get_db
-from app.services.match_services import generate_room_code
+from app.services.match_services import MatchService
+from app.core.dependencies import get_current_player
+from app.models.player import Player
 
 
 router = APIRouter()
 
 @router.post("/api/matches/create-invite")
-def create_invited_match(db: Session=Depends(get_db)):
-    code = generate_room_code()
-    
+def create_invited_match(db: Session=Depends(get_db), current_player: Player=Depends(get_current_player) ):
+    room_code = MatchService.create_invite_match(db, current_player.id)
+    return {"room code":room_code}
--- app/services/match_service.py
+++ app/services/match_service.py
@@ -1,7 +1,22 @@
 import random
 import string
+from app.models.match import Match
 
-def generate_room_code(length=6):
-    pool = string.ascii_uppercase + string.digits
-    return ''.join(random.choice(pool) for _ in range(length))
+class MatchService:
+    
+    @staticmethod
+    def _generate_room_code(length=6):
+        pool = string.ascii_uppercase + string.digits
+        return ''.join(random.choice(pool) for _ in range(length))
 
+    @staticmethod
+    def create_invite_match(db, user_id):
+        room_code = MatchService._generate_room_code()
+        new_invite_match = Match(player1_id=user_id,
+                                 status="WAITING",
+                                 invite_code=room_code)
+        db.add(new_invite_match)
+        db.commit()
+        db.refresh(new_invite_match)
+
+        return room_code
