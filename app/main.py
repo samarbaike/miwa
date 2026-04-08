f272ee27f8c92ce8634e91d83bd5c92c51559045
samarbaike <samarb16k@gmail.com>
2026-04-08 11:49:59 +0600

    implemented the MatchmakingPool in main.py

--- app/main.py
+++ app/main.py
@@ -1,21 +1,41 @@
 from fastapi import FastAPI
+import asyncio
+from contextlib import asynccontextmanager
+
 from app.routes import auth, user,  matches, ws
 from app.database import engine, Base
-
 from app.models.user import Player
 from app.models.session import SessionTable
+from app.services.matchmaking_service import MatchmakingPool
 
 print("INFO:     Connecting to Supabase...")
 Base.metadata.create_all(bind=engine)
 print("INFO:     Tables created successfully!")
 
-app = FastAPI()
+miwa_pool = MatchmakingPool()
+
+async def run_matchmaking():
+    while True:
+        miwa_pool.tick()
+        while True:
+            match = miwa_pool.try_pair()
+            if match is None:
+                break
+            print(f"Match found in background: {match[0].player.username} and {match[1].player.username}")
+        await asyncio.sleep(10)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    asyncio.create_task(run_matchmaking())
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(matches.router)
 app.include_router(ws.router)
-
+    
 @app.get("/")
 async def root():
     return "Quiz App is sprinting"
