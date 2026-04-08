9405bc1ebc0b51b57eea35057f6dc17f63b38b1d
samarbaike <samarb16k@gmail.com>
2026-04-08 20:06:44 +0600

    got some issues going on with websocket... job for tmrw

--- app/routes/matches.py
+++ app/routes/matches.py
@@ -11,7 +11,8 @@ from app.services.matchmaking_service import miwa_pool, WaitingPlayer
 router = APIRouter()
 
 @router.post("/api/matches/create-invite")
-def create_invited_match(db: Session=Depends(get_db), current_player: Player=Depends(get_current_player) ):
+def create_invited_match(db: Session=Depends(get_db), 
+                         current_player: Player=Depends(get_current_player) ):
     room_code = MatchService.create_invite_match(db, current_player.id)
     return {"room_code":room_code}
 
@@ -24,14 +25,17 @@ def get_match_status(id: int, db: Session=Depends(get_db)):
     return match
 
 @router.post("/api/matchmaking/join")
-def matchmaking(category: str, current_player: Player = Depends(get_current_player), db: Session = Depends(get_db)):
+def matchmaking(category: str, 
+                current_player: Player = Depends(get_current_player), 
+                db: Session = Depends(get_db)):
     #1st guard
     for player in miwa_pool.q:
         if player.get_id() == current_player.id:
             raise HTTPException(status_code=400, detail="Oyunchu echak ele kutuu bolmosundo")
     
     #2nd guard
-    exists = db.query(Match).filter(Match.status == "active", (current_player.id == Match.player1_id) | (current_player.id == Match.player2_id)).first()
+    exists = db.query(Match).filter(Match.status == "active", 
+                                    (current_player.id == Match.player1_id) | (current_player.id == Match.player2_id)).first()
     if exists is not None:
         raise HTTPException(status_code=400, detail="Oyunchu echak ele oyunda")
     
@@ -40,6 +44,8 @@ def matchmaking(category: str, current_player: Player = Depends(get_current_play
     miwa_pool.enqueue(player)
     match = miwa_pool.try_pair()
     if match is None:
-        return {"status":"searching", "message":"Kutuu bolmosuno koshtuk. Ataandash kutuudobuz."}
+        return {"status":"searching", 
+                "message":"Kutuu bolmosuno koshtuk. Ataandash kutuudobuz."}
     else:
-        return {"status":"match_found", "message":f"{match[1].player.username} menen oiun bashtaluuda."}
+        return {"status":"match_found", 
+                "message":f"{match[1].player.username} menen oiun bashtaluuda."}
