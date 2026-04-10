38beeacfd7aa88309fa812cfc9d6b954e69dd920
samarbaike <samarb16k@gmail.com>
2026-04-10 18:17:56 +0600

    done with joining to match with invite code

--- app/routes/matches.py
+++ app/routes/matches.py
@@ -5,7 +5,7 @@ from app.database import get_db
 from app.services.match_service import MatchService
 from app.core.dependencies import get_current_player
 from app.models.user import Player
-from app.models.match import Match, MatchStatus
+from app.models.match import Match, MatchStatus, MatchMode
 from app.services.matchmaking_service import WaitingPlayer
 
 router = APIRouter()
@@ -15,6 +15,25 @@ def create_invited_match(db: Session=Depends(get_db),
                          current_player: Player=Depends(get_current_player) ):
     room_code = MatchService.create_invite_match(db, current_player.id)
     return {"room_code":room_code}
+
+@router.post("/api/matches/join-invite")
+def join_bycode(room_code: str,
+                db: Session = Depends(get_db),
+                current_player: Player = Depends(get_current_player)):
+    
+    match = db.query(Match).filter(Match.mode==MatchMode.INVITE, Match.invite_code==room_code).first()
+    if match is None:
+        raise HTTPException(status_code=400, detail="Myndai kodd menen ec kandai oyun jok")
+
+    if current_player.id == match.player1_id:
+        raise HTTPException(status_code=400, detail="Siz ozunuzdun oyunga ozunuz koshula albaisyz")
+        
+    match.player2_id = current_player.id
+    match.status = MatchStatus.IN_PROGRESS
+    db.commit()
+    db.refresh(match)
+
+    return match
 
 
 @router.get("/api/match/{id}")
@@ -66,3 +85,4 @@ def cancel(request: Request,
         "status":"success",
         "message":"Kutuu bolmusunon chygaryldynyz"
     }
+
