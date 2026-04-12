c0f1fae0a47f46e2a50d0c262b21f3b76d29bcb5
samarbaike <samarb16k@gmail.com>
2026-04-12 11:17:54 +0600

    started websocket game service routing

--- app/routes/ws.py
+++ app/routes/ws.py
@@ -1,4 +1,12 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from sqlalchemy.orm import Session
+from sqlalchemy import func, select
+import asyncio
+
+from app.core.events import WSEvents
+from app.models.question import MultipleChoice
+from app.database import engine
+from app.services.game_service import GameService
 
 router = APIRouter()
 
@@ -17,14 +25,54 @@ async def websocket_endpoint(websocket: WebSocket, match_id: int, player_id:int)
         #Keepin' the line open forever, waiting for messages
         while True:
             # Wait for a message from this specific player
-            data = await websocket.receive_json()
+            message = await websocket.receive_json()
+            event = message.get("event")
+            data = message.get("data", {})
+            # provided that user sends json in 
+            # {"event" : "SUBMIT_ANSWER", "data" : {"answer" : 2}}
+                
+            if event == WSEvents.JOIN_MATCH:
             
-            #just bounceing the message back to everyone in the room
-            await room_manager.broadcast(match_id, data)
-            
+                #just bounceing the message back to everyone in the room
+                await room_manager.broadcast(match_id, {
+                    "event" : WSEvents.PLAYER_JOINED,
+                    "player_id" : player_id
+                    })
+
+                if (
+                    len(room_manager.active_connections[match_id]) == 2 and
+                    match_id not in websocket.app.state.active_games
+                ):
+                    with Session(engine) as db:
+                        query = select(MultipleChoice.id).order_by(func.random()).limit(10)
+                        q_ids = db.execute(query).scalars().all()
+
+                    player1_id, player2_id = list(room_manager.active_connections[match_id].keys())
+                    game = GameService(
+                        match_id,
+                        player1_id,
+                        player2_id,
+                        q_ids
+                    )
+
+                    websocket.app.state.active_games[match_id] = game
+
+                    await game.start_question(websocket.app)
+
+            elif event == WSEvents.SUBMIT_ANSWER:
+                answer = data.get("answer")
+                #timestamp to calculate time_taken for correct answers
+                timestamp = asyncio.get_event_loop().time()
+
+                #fetching game object from active games
+                game = websocket.app.state.active_games[match_id]
+
+                #calling handle answer from GameService
+                await game.handle_answer(player_id, answer, timestamp, websocket.app)
+
     except WebSocketDisconnect:
         # 3. If the player's internet drops, tell the manager to disconnect them
-        room_manager.disconnect(match_id, player_id)
+        await room_manager.disconnect(match_id, player_id)
 
 @router.websocket("/ws/lobby/{player_id}")
 async def lobby_websocket(websocket: WebSocket, player_id: int):
@@ -38,7 +86,7 @@ async def lobby_websocket(websocket: WebSocket, player_id: int):
     await global_manager.connect_player(player_id, websocket)
     
     try:
-        # Keep connection open waiting for server notifications (like match_found)
+        # Keep connection open waiting for server natifications (like match_found)
         while True:
             await websocket.receive_text() 
     except WebSocketDisconnect:
